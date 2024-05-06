329293109da24a7ca8fff32f6677906d1a7332ec
Daniel <168070901+danielm3600@users.noreply.github.com>
2024-05-06 13:26:25 +1000

    Add files via upload

---
+++ content.js
@@ -0,0 +1,40 @@
+let gain_node;
+
+const createGainNodeFromAudioContext = (mediaStream) => {
+    const audioContext = new AudioContext();
+    const mediaElementAudioSourceNode = audioContext.createMediaElementSource(mediaStream);
+    const node = audioContext.createGain();
+
+    node.gain.value = 1;
+    mediaElementAudioSourceNode.connect(node);
+    node.connect(audioContext.destination);
+
+    return node;
+}
+
+const setup = () => {
+    const mediaStream = document.querySelector("video");
+
+    if (!mediaStream) {
+        return null;
+    }
+
+    const node = createGainNodeFromAudioContext(mediaStream);
+
+    return node;
+}
+
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.id == 1) {
+        gain_node = gain_node || setup();
+
+        if(gain_node == null){
+            sendResponse("No MediaStream");
+            return;
+        }
+
+        gain_node.gain.value = Number(request.message);
+        sendResponse("ok");
+    }
+});
